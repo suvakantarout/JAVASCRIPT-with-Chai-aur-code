d474788a1236445f192283f5d52eef4fac365c75
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-01-25 23:23:53 +0530

    25.01.2025

---
+++ 02_Data_types/data_types.js
@@ -0,0 +1,13 @@
+console.log("DATA TYPES")
+
+let name = "suvakant";  // string type
+let age = 19;    //Integer type
+let isloggin = false;  //boolean type true /false
+let state = null;
+let city
+
+
+
+console.table([name,age,isloggin,state,city]);
+
+
