fcbc9da56a7840afad4c3b37367f26289c52d96a
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-01-25 23:41:48 +0530

    25.01.2025

---
+++ 01_Basics/2_data_types.js
@@ -0,0 +1,10 @@
+
+console.log("DATA TYPES")
+
+let name = "sunit";  //string type
+let age = 19;    //Integer type
+let isloggedin = true;   //boolean type
+let id = null;    //Null type
+let marks       //undefind type
+
+console.table([name,age,isloggedin,id,marks]);
