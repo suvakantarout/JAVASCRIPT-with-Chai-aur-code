972b7ba6fb2ce9623773ab90f3516773cac92f85
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-01-28 23:14:57 +0530

    28.01.2025

---
+++ 01_Basics/math_Operations.js
@@ -0,0 +1,12 @@
+console.log("1" + 2);  // 12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(2 + 2 + "2");  //42
+
+
+console.log(+true);
+
+let count = 100;
+count++;
+++count;
+console.log(count);
