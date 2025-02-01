a70313299a56ce275626c06464d51fb8d587cf09
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-02 00:37:23 +0530

    02.02.2025

--- 01_Basics/7_.js
+++
@@ -1,6 +0,0 @@
-const name = "Sunit"
-const repocount = 6
-
-// console.log(name + repocount)  // old method
-
-console.log(`Hello my name is ${name} and my repo count is ${repocount}`);  // New method in javascript
---
+++ 01_Basics/String.js
@@ -0,0 +1,30 @@
+const name = "Sunit"
+const repocount = 6
+
+// console.log(name + repocount)  // old method
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);  // New method in javascript
+
+
+let name2 = new String ("Suvakanta rout");   //Dynamically allocate the string
+console.log(name2[1]);
+console.log(name2[5])
+console.log(name2[9])
+
+// STRING FUNCTIONS
+console.log(name2.length); //Return the length of the string
+console.log(name2.toUpperCase());  //Convert to uppercase
+console.log(name2.charAt(10));  //Return the character 
+console.log(name2.indexOf('t'));  //reture the index of a perticulsr chsrscter
+console.log(name2.substring(0, 8));  //printing substring from one index to another index
+console.log(name2.slice(0,4));  //split the required string
+
+const str3 = "     suv     ";
+console.log(str3);
+console.log(str3.trim()); // Trim the spaces from start as well as end of a string.
+
+
+const str4 = "My name is suv";
+console.log(str4.split(' '));  // Split a string through sapce
+
+
