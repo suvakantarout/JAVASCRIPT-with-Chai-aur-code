1d71ef61a6311ad19ddc7871414de1931fdddbf2
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-06 22:47:57 +0530

    06.02.2025

---
+++ Module-2/1_Array.js
@@ -0,0 +1,12 @@
+
+let arr1 = [1,2,3,4,5,6];
+let arr2 = ["suv","swagat"];
+console.log(arr1);
+console.log(arr2);
+console.log(arr1[1])
+console.log(arr2[1])
+
+
+// Array methods
+arr1.push(10);
+console.log(arr1);
