eec3f0bd065e8b5c69cf4ae3fe392b6632c11e9a
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-07 21:52:29 +0530

    07-02-2025

--- Module-2/1_Array.js
+++ Module-2/1_Array.js
@@ -1,3 +1,4 @@
+// Array => In javascript array is Dynamic in terms of ResizeObserverSize, like vector
 
 let arr1 = [1,2,3,4,5,6];
 let arr2 = ["suv","swagat"];
@@ -8,5 +9,18 @@ console.log(arr2[1])
 
 
 // Array methods
-arr1.push(10);
+arr1.push(10);  //Push element at the end.
 console.log(arr1);
+
+arr1.pop();   // pop elemet from the end.
+console.log(arr1);
+
+arr1.unshift(111);  // Add element at the starting of the array. by shifting all the elements.
+console.log(arr1);
+
+arr1.shift();   //Remove the unshift element.
+console.log(arr1)
+
+console.log(arr1.indexOf(3));
+console.log(arr1.includes(3)); //If any index having the value 3 then it will return true else false.
+
