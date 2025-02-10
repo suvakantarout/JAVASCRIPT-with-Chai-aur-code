b4643ae32c8c726583373a93bb347b8f3cb81772
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-10 20:23:34 +0530

    Object.js

---
+++ Module-2/5_obj_de_structure.js
@@ -0,0 +1,13 @@
+// De-Structure of Objects
+
+const course = {
+    C_name: "JS Hindi",
+    price: 1000,
+    C_Instructor: "Hitesh"
+}
+console.log(course.C_Instructor);
+
+// Another easy way
+const {C_Instructor: C_Ins} = course  //you can coustomise and use the keys any where in the  program 
+// without using any obj name and (.) operator
+console.log(C_Ins);
