27e15dea5f57852502aae670cd058e748314d9b7
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-10 22:13:01 +0530

    API_Intro

--- Module-2/5_obj_destructure.js
+++ Module-2/5_obj_destructure.js
@@ -11,3 +11,15 @@ console.log(course.C_Instructor);
 const {C_Instructor: C_Ins} = course  //you can coustomise and use the keys any where in the  program 
 // without using any obj name and (.) operator
 console.log(C_Ins);
+
+
+// APIs
+// APIs asways comes in the form of empty object {} / form of array.
+
+// Ex: Of an API
+{
+    id:241515345
+    "name" = "Suv"
+    location: "CUTTACK"
+
+}
