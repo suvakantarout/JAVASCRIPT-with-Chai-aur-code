ebc8d2ad680ce5eb77fda1b40e3f0881567379e8
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-11 23:00:05 +0530

    Js functon Basics

---
+++ Module-3/1_Functions.js
@@ -0,0 +1,24 @@
+// Function syntax
+
+// function name (Prameters){
+//     //func Body
+// }
+
+// func call
+// name(Arguments / values);
+
+
+// Function without reaturn type / Doesn't returning any thing
+function sum(num1,num2){
+    console.log(num1 + num2)
+}
+sum(5, 5)
+
+
+// Function with return type
+function sum(num1,num2){
+    return num1 + num2;
+}
+
+let ans = sum(10,10)
+console.log(ans);
