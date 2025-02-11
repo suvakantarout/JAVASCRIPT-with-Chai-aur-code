fede678dcd4871ffc8e91577466f3e5ee0d71c68
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-11 23:51:47 +0530

    Object as an argument

--- Module-3/1_Functions.js
+++ Module-3/1_Functions.js
@@ -30,3 +30,26 @@ function calculatecartprice(...num1){  // (...)Rest operator (add the numbers/co
 }
 
 console.log(calculatecartprice(200,400,300,500))
+
+
+
+// Pass Object to an function argument
+const user = {
+    username: "Suv",
+    price: 199
+}
+
+function handelObject (anyobj){
+    return anyobj;
+}
+
+console.log(handelObject(user));
+
+
+// pass an array to an funcction argumrnt.
+
+let arr = [200,400,300,100];
+
+function returnSecondvalue(getArray){
+    
+}
