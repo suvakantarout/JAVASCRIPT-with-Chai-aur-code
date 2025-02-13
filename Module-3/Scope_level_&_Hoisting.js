cfa3fb1e84e2ed19fc7a99c6ac3a43522051805d
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-13 20:43:24 +0530

    13-02-2025

---
+++ Module-3/Scope_level_&_Hoisting.js
@@ -0,0 +1,11 @@
+
+if(true){
+    const username = "Suv";
+    if(username == "Suv"){
+        const website = "Suv's blog";
+        console.log(username + website);
+    }
+    // console.log(website); // Error
+}
+
+// console.log(username);  //Error
