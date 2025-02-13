3324b58628b5b88ef114c97c53e58ab875e803ef
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-13 22:33:02 +0530

    IIFE in js

---
+++ Module-3/IIFE.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expression (IIFE)
+
+// IIFE ==> IIFE is used to reduce the Global scope polution from the program.
+
+// IIFE is generally used to avoid function calling Globally
+
+// IIFE ==> ()()
+// () => Is for argument
+// () => Is for the entire function
+
+(function DBC(){
+    console.log("DB CONNECTED");
+})()
+
+// console.log(DBC());
