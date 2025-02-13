5dc0a5803a16f3b775d4117c86d5bd04b7c40dee
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-13 21:17:14 +0530

    Scope level & Hoisting

--- Module-3/Scope_level_&_Hoisting.js
+++ Module-3/Scope_level_&_Hoisting.js
@@ -2,10 +2,27 @@
 if(true){
     const username = "Suv";
     if(username == "Suv"){
-        const website = "Suv's blog";
+        const website = " Suv's blog";
         console.log(username + website);
     }
     // console.log(website); // Error
 }
 
 // console.log(username);  //Error
+
+
+
+console.log(addOne(5));  //will execute successfully, Cause when a functon called it immediately
+// go to its defination.
+function addOne(num){
+    return num + 1;
+}
+
+
+
+console.log(addTwo(5)); //will give an error(Access 'addTwo' before initialization)
+const addTwo = function(num){ // Hoisting => addTwo hostes the Function
+    return num + 2;
+}
+
+
