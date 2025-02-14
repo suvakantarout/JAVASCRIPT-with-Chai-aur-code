53a2670b1164d5ee93216e07b56e36088eb4122f
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-14 22:31:26 +0530

    14-02-2025

--- Module-3/This_&_Arrow_func.js
+++ Module-3/This_&_Arrow_func.js
@@ -1,14 +1,17 @@
-// Arrow functions
+// This keyword => Refers to the current context.
 
 const user = {
     username: "Suv",
     price: 999,
 
     welcomeMessage: function(){
-        console.log(`$(this.username), welcome to website`);
+        console.log(`${this.username}, welcome to website`);
     }
 }
 
-console.log(user.welcomeMessage());
+user.welcomeMessage();
+user.username = "Sam"
+user.welcomeMessage();
+
 
 console.log(this); // Interview
