b3eac52d329587c9d0a11142dd36539eaa9ebad4
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-15 22:30:34 +0530

    .

--- Module-3/Control_flow.js
+++ Module-3/Control_flow.js
@@ -8,3 +8,40 @@ if(isLoggedin){
 else{
     console.log("No")
 }
+
+// conditions => <, >, <=, >=, ==, !=, ===, !==
+
+if(2 === "2"){   // type of both element are not same.(===(strict equal))
+    console.log("True")
+}
+else{
+    console.log("False");
+}
+
+let temp = 41;
+if(temp === 41){
+    console.log("warm")
+}
+else{
+    console.log("cool")
+}
+
+
+// Interview :
+
+// falsy values => false, 0, -0, NAN, undefined, "", null, Bigint
+// Truthy values => true, 1, "0", "false", " ", [], function(){}
+
+
+
+// Nullish coalescing operator(??)
+let val
+val = 5 ?? 10
+val2 = null ?? 10
+val3 = undefined ?? 10
+val4 = null ?? 10 ?? 20
+
+console.log(val)
+console.log(val2)
+console.log(val3)
+console.log(val4)
