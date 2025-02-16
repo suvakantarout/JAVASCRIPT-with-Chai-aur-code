831b040faf86a98b950af08514d402c151641648
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-16 20:58:10 +0530

    Multiplication table

--- Module-3/7_Loops.js
+++ Module-3/7_Loops.js
@@ -1,15 +1,24 @@
 
-for(let i = 0; i<=10; i++){
-    if(i === 5){
-        console.log("5 is best number");
-    }
-    console.log(i);
-}
+// for(let i = 0; i<=10; i++){
+//     if(i === 5){
+//         console.log("5 is best number");
+//     }
+//     console.log(i);
+// }
 
 
+// for(let i = 0; i<=10; i++){
+//     console.log("Inner loop value",i);
+//     for(let j =0;j<=10; j++){
+//         console.log(`Inner loop value ${i} and outer loop value ${j}`);
+//     }
+// }
+
+
+// Multiplication table
 for(let i = 0; i<=10; i++){
-    console.log("Inner loop value",i);
-    for(let j =0;j<=10; j++){
-        console.log(`Inner loop value ${i} and outer loop value ${j}`);
+    console.log(`Multiplication table of ${i}: `);
+    for(let j = 0; j<=10; j++){
+        console.log(i +'*' + j + '=' +  i*j);
     }
 }
