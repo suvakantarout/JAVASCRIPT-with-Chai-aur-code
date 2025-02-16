e7083c39f022588b0adcbed11a01fc51c300d27a
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-16 22:35:44 +0530

    For off Loop

---
+++ Module-3/More_Loops.js
@@ -0,0 +1,15 @@
+// For off Loop
+// Using Array
+let arr = [1,2,3,4,5];
+
+for(const num of arr){
+    console.log(num);
+}
+
+
+// Using String
+let str = "Hello world";
+
+for(const text of str){
+    console.log(`Greetings for the day: ${text}`)
+}
