507389e10809d708ff5b3f4dfd5dbce9e1cc47e5
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-16 21:19:33 +0530

    braek & continue

--- Module-3/7_Loops.js
+++ Module-3/7_Loops.js
@@ -19,6 +19,26 @@
 for(let i = 0; i<=10; i++){
     console.log(`Multiplication table of ${i}: `);
     for(let j = 0; j<=10; j++){
-        console.log(i +'*' + j + '=' +  i*j);
+        // console.log(i +'*' + j + '=' +  i*j);
+        console.log(`${i} * ${j} = ${i *j}`)
     }
+}
+
+
+// Break
+for(let i = 0; i<=10; i++){
+    if(i === 5){
+        console.log("Detected break")
+        break
+    }
+    console.log(i);
+}
+
+// continue
+for(let i = 0; i<=10; i++){
+    if(i === 5){
+        console.log("Detected break")
+        continue
+    }
+    console.log(i);
 }
