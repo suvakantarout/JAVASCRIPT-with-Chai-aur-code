82640d78969e8187d627af6e628a33e65084a06b
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-23 20:22:50 +0530

    project-2

---
+++ PROJECTS/P2/p2.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form");
+form.addEventListener('submit',(S) =>{
+    S.preventDefault();  //Not goung to another page, instread stay in that page and calculate the ans.
+    const height = parseInt(document.querySelector("#Height").value); 
+    const weight = parseInt(document.querySelector("#Weight").value);   //value returns in string(parseInt is used to convert it into string)
+    const Results = (document.querySelector("#results"));
+
+    if(height === '' || height<0 || isNaN(height)){  //isNAN() => check height is a number or not(return true/false)
+        Results.innerHTML = "Please give a valid height";
+    }
+    else if(weight === '' || weight<0 || isNaN(weight)){
+        Results.innerHTML = "Please give a valid height";
+    }
+    else{
+        const bmi = (weight / ((height*height)/10000)).toFixed(2);
+        Results.innerHTML = `${bmi}`;
+    }
+});
