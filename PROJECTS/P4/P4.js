d44ae045f21803941fd0e2befa83e46e4e119d91
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-27 16:07:57 +0530

    27-02-2025

---
+++ PROJECTS/P4/P4.js
@@ -0,0 +1,24 @@
+const randomNumber = (parseInt(Math.random() * 10 + 1));  //it will give decimal value so we nned to convert it into integer (parseInt)
+
+const submit = document.querySelector('#button');
+const userinput = document.querySelector('#number_input');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.Result');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
