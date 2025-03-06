1fa9951571ed0cc8e1cba6ee5c221b04d2d8b30b
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-03-06 22:04:15 +0530

    fetch exaple

--- Module-5/4_Fetch.js
+++ Module-5/4_Fetch.js
@@ -1,17 +1,21 @@
 
 // fetch Example
-fetch('api.github/users/suvakanta rout') //request
+
+
+fetch('https://randomuser.me/api/') //request
 // gete the response
 .then((response) => {
-    return response.json();
+    return response.json(); //will get the response in json format(also -> .text())
 })
 // handel the response
 .then( (data) => {
     console.log(data);
 })
+// catch if any error occured
 .catch((error) =>{
     console.log("Error occured");
 })
+// finally will execute always
 .finally( () => {
-    console.log("The response will either resolve or reject");
+    console.log("The response will either resolve or reject.");
 })
