633eecb4583f6beb1993a129a3b56b123a2f9e04
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-03-10 20:05:40 +0530

    added

--- Module-6/3_Constructor.js
+++ Module-6/3_Constructor.js
@@ -1,21 +1,43 @@
-class user {
+// class user {
 
-    constructor(username, email, password){
+//     constructor(username, email, password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+//     encryptPassword(){   //encrypt method
+//         return `${this.password}abc`
+//     }
+
+//     usernameToUppercase(){
+//         return `${this.username.toUpperCase()}`
+//     }
+
+// }
+
+// const chai = new user("suv","chai@gmail.com", "123");
+// console.log(chai.encryptPassword());
+// console.log(chai.usernameToUppercase());
+
+
+// Inheritance
+
+class user {
+    constructor(username){
         this.username = username;
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+} 
+
+class Teacher extends User{
+    constructor(username,email,password){
+        super(username);
         this.email = email;
         this.password = password;
-    }
 
-    encryptPassword(){   //encrypt method
-        return `${this.password}abc`
     }
-
-    usernameToUppercase(){
-        return `${this.username.toUpperCase()}`
-    }
-
 }
-
-const chai = new user("suv","chai@gmail.com", "123");
-console.log(chai.encryptPassword());
-console.log(chai.usernameToUppercase());
