bbc61dc12833772a7cd0577f45f30d8481ec52b9
Suvakanta Rout <suvakantarout2020@gmail.com>
2025-03-28 22:16:47 +0530

    initial

---
+++ PROJECTS/LeetCode Matrix/JS/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const searchButton = document.getElementById("search_button"); //id
+    const username = document.getElementById("userInput"); //id
+    const userdatacontainer = document.getElementsByClassName("user_data_contaner") 
+    const easyprogressCircle = document.getElementsByClassName("easy-progress");
+    const midprogressCircle = document.getElementsByClassName("mid-progress");
+    const hardprogressCircle = document.getElementsByClassName("hard-progress");
+    const easylabel = document.getElementById("easy_label");
+    const midlabel = document.getElementById("mid_label");
+    const hardlabel = document.getElementById("hard_label");
+    const carddatacontainer = document.querySelector(".data_contaiiner")  //its a class(so i added .)
+})  
