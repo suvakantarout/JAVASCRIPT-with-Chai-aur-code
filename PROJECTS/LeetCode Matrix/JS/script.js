c16a25784d6d3f0ae51a118c4b99cd5bb43ada7d
Suvakanta Rout <suvakantarout2020@gmail.com>
2025-03-28 23:27:50 +0530

    initial

--- PROJECTS/LeetCode Matrix/JS/script.js
+++ PROJECTS/LeetCode Matrix/JS/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded",function(){
     const searchButton = document.getElementById("search_button"); //id
-    const username = document.getElementById("userInput"); //id
+    const usernameInput = document.getElementById("userInput"); //id
     const userdatacontainer = document.getElementsByClassName("user_data_contaner") 
     const easyprogressCircle = document.getElementsByClassName("easy-progress");
     const midprogressCircle = document.getElementsByClassName("mid-progress");
@@ -9,4 +9,15 @@ document.addEventListener("DOMContentLoaded",function(){
     const midlabel = document.getElementById("mid_label");
     const hardlabel = document.getElementById("hard_label");
     const carddatacontainer = document.querySelector(".data_contaiiner")  //its a class(so i added .)
+
+
+
+    // searchButton.addEventListener('click', function() {
+    //     const username = usernameInput.Value;
+    //     console.log("Loggin username: ",username);
+    // });
+        searchButton.addEventListener('click', () => {
+        const username = usernameInput.value;
+        console.log("Loggin username: ",username);
+    });
 })  
